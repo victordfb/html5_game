0fefda099c69852cec31ebf0e1d6d3499181274b
Victor Dolirio <victordfb@gmail.com>
2012-11-08 11:33:29 -0200

    Reorganizacao de pacotes

--- js/game.js
+++ js/game.js
@@ -73,32 +73,3 @@ GameEngine.prototype = {
         this.drawRunner();
     }    
 }
-
-function Html5GameLoop(gameEngine){
-    this.gameEngine = gameEngine;
-}
-Html5GameLoop.prototype = {
-    animate: function(now) { 
-        html5GameLoop.gameEngine.updateFrame(now);
-        this.requestNextAnimationFrame(html5GameLoop.animate);
-    },
-    startGame: function() {
-        requestNextAnimationFrame(this.animate);
-    }
-}
-
-var canvas = document.getElementById('game-canvas');
-var gameEngine = new GameEngine(canvas, new StageOne());
-var html5GameLoop = new Html5GameLoop(gameEngine);
-
-html5GameLoop.startGame();
-
-window.onkeydown = function(e){
-    var key = e.keyCode;
-    if(key == 39){
-        gameEngine.moveToLeft();
-    }
-    if(key == 37){
-        gameEngine.moveToRight();
-    }
-}
